925dfc6cdae7d2e9bac6eddcc1ec9359c1607252
asdfmi <nattogohan710580@gmail.com>
2025-11-12 21:43:12 +0900

    refactor extract text

--- domain/value-objects/node-configs/extract-text-config.js
+++ domain/value-objects/node-configs/extract-text-config.js
@@ -3,10 +3,11 @@ import { requireNonEmptyString } from "../../utils/validation.js";
 export default class ExtractTextConfig {
   constructor(rawConfig) {
     const config = rawConfig && typeof rawConfig === "object" ? rawConfig : {};
-    const xpath = typeof config.xpath === "string" ? config.xpath : "";
-    const as = typeof config.as === "string" ? config.as : "";
-    this.xpath = requireNonEmptyString(xpath, "extract_text config.xpath");
-    this.as = requireNonEmptyString(as, "extract_text config.as");
+    this.xpath = requireNonEmptyString(
+      config.xpath,
+      "extract_text config.xpath",
+    );
+    this.as = requireNonEmptyString(config.as, "extract_text config.as");
     Object.freeze(this);
   }
 
